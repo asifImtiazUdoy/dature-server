216c3008b903710a11e11f3e3cd88e1f606041eb
asif-imtiaz-udoy <asifimtiaz240@gmail.com>
2022-11-10 23:14:13 +0600

    my review modified

--- index.js
+++ index.js
@@ -41,7 +41,16 @@ async function run() {
         });
 
         app.get('/reviews', async (req, res) => {
-            
+            let query = {};
+
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const review = await cursor.toArray();
+            res.send(review);
         });
 
         app.get('/reviews/:id', async (req, res) => {
